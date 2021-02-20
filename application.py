e6f152b1d813c2af7f21d4673f4e9b1cc1a20f97
Parsa Saeedi <psaeedi1381@gmail.com>
2021-02-20 12:43:35 -0800

    Created the repository and setup the web application

---
+++ application.py
@@ -0,0 +1,40 @@
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# from helpers import apology, login_required
+
+import requests
+import urllib.parse
+from functools import wraps
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+#  Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///speedtyping.db")
+
+
+# Index
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # Write code here
+    # return render_template("index.html")
+    return "Hello"
