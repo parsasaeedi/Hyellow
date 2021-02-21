f55fa9d9319943e339a08471456a00ec63baa5c9
Carl-Joshua <79351778+Carl-Joshua@users.noreply.github.com>
2021-02-21 14:22:34 +0700

    add questionnaireMBTI

--- application.py
+++ application.py
@@ -40,15 +40,29 @@ def index():
     return "Hello"
 
 
-@app.route("/questionnaireMBTI")
-def questionnaire():
+@app.route("/questionnaire/1")
+def MBTI():
     MBTI_questions = [
         "After a tiring weekend you recharge by being with people instead of being by yourself",
         "Instead of thinking about present details, you'd rather think of future possibilities",
         "You analyze the problem to make decisions instead of just trusting your gut",
         "You'd rather plan your birthday compared to celebrating it spontaneously"]
+    test = "MBTI"
     if request.method == "GET":
-        return render_template("questionnaireMBTI.html", MBTI_questions = MBTI_questions)
+        return render_template("questionnaireMBTI.html", questions = MBTI_questions, test = test)
+    # add else statement if method == 'post' and return value of the personality type etc.
+
+
+@app.route("/questionnaire/2")
+def ColorTest():
+    MBTI_questions = [
+        "After a tiring weekend you recharge by being with people instead of being by yourself",
+        "Instead of thinking about present details, you'd rather think of future possibilities",
+        "You analyze the problem to make decisions instead of just trusting your gut",
+        "You'd rather plan your birthday compared to celebrating it spontaneously"]
+    test = "MBTI"
+    if request.method == "GET":
+        return render_template("questionnaireMBTI.html", questions=MBTI_questions, test=test)
     # add else statement if method == 'post' and return value of the personality type etc.
 
 
